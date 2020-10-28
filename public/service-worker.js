5cad0b56ed7c18b1aed85c7a9b779b41938edf44
AK <icecicle04@gmail.com>
2020-10-27 23:57:57 -0400

    updated service worker file to remove errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,10 +10,8 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png",
     "styles.css"
   ];
-​
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
-​
 // install
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
@@ -36,10 +34,8 @@ self.addEventListener("install", function (evt) {
         console.log(err);
       })
   );
-​
   self.skipWaiting();
 });
-​
 // activate
 self.addEventListener("activate", function (evt) {
   evt.waitUntil(
@@ -54,10 +50,8 @@ self.addEventListener("activate", function (evt) {
       );
     })
   );
-​
   self.clients.claim();
 });
-​
 // fetch
 self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/")) {
@@ -81,10 +75,8 @@ self.addEventListener("fetch", function (evt) {
         })
         .catch((err) => console.log(err))
     );
-​
     return;
   }
-​
   evt.respondWith(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.match(evt.request).then((response) => {
